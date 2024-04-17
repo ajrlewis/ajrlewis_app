52bf4d983809f52e7a0212dd7fb50a7e6e940465
ajrlewis <hello@ajrlewis.com>
2024-04-17 20:39:36 +0100

    Updated using the prod_config class.

--- scripts/create_db_tables.sh
+++ scripts/create_db_tables.sh
@@ -6,11 +6,12 @@ import sys
 sys.path.append("./")
 
 from app import create_app, db
+from configs.prod_config import ProdConfig
 from models.user import User
 from models.client import Client
 from models.invoice import Invoice
 
-app = create_app()
+app = create_app(ProdConfig)
 with app.app_context():
     db.create_all()
     db.session.commit()
