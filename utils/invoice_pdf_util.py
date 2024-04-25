83db9cfa716bba03787af7729b3fdd9d894de5ed
ajrlewis <hello@ajrlewis.com>
2024-04-25 22:04:08 +0100

    Removed trailing quotation marks.

--- utils/invoice_pdf_util.py
+++ utils/invoice_pdf_util.py
@@ -135,7 +135,7 @@ Replication of client data or ideas without permission is strictly prohibited.
 A.J.R. Lewis and the client agree to a 50% downpayment of the quoted amount at the start of their collaboration.
 The remainder will be paid at the time of delivery of the agreed work.
 Both parties agree that an on-chain transaction to the above address with 3 confirmations constitutes as payment received by A.J.R. Lewis.
-It is the responsibility of A.J.R. Lewis to ensure correct payment details are sent to the client."
+It is the responsibility of A.J.R. Lewis to ensure correct payment details are sent to the client.
     """
     )
     pdf.line_break()
@@ -147,7 +147,7 @@ It is the responsibility of A.J.R. Lewis to ensure correct payment details are s
 A.J.R. Lewis retains ownership of any software created for the client until full payment is made.
 Upon full payment for the completed work, A.J.R. Lewis agrees to transfer to the client the copyright of the aforementioned work, including but not limited to all rights of reproduction, distribution, public performance, and adaptation, in all fields of exploitation, both currently known and hereafter devised.
 This transfer is triggered by the receipt of the full payment by A.J.R. Lewis (as defined above) and should take place immediately thereafter.
-A.J.R. Lewis shall execute all documents and take all actions necessary to effectuate such copyright transfer to the client, ensuring the client acquires full and exclusive rights to the work for its entire duration of copyright under the law."
+A.J.R. Lewis shall execute all documents and take all actions necessary to effectuate such copyright transfer to the client, ensuring the client acquires full and exclusive rights to the work for its entire duration of copyright under the law.
 """
     )
     pdf.line_break()
